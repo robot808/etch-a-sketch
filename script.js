7ed51791391f847f7feb261ab8274a2a78b3b9b8
robot808 <rsbradshaw88@protonmail.com>
2023-04-29 21:00:37 -0500

    Add clearBoard function and button to redraw board

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+// remove all children of board div
+function clearBoard () {
+  const board = document.querySelector("#board");
+  while (board.hasChildNodes()) {
+    board.removeChild(board.firstChild);
+  }
+}
+
 // given number of squares high and wide, draw a squares by squares grid of divs
 function drawBoard (squares) {
-  //rows
+  const board = document.querySelector("#board");
+
+  // rows
   for (let i = 0; i < squares; i++) {
-    let board = document.querySelector("#board");
-    let row = document.createElement("div");
+    const row = document.createElement("div");
     row.classList.add("row");
 
-    //columns
+    // columns
     for (let j = 0; j < squares; j++) {
-      let square = document.createElement("div");
+      const square = document.createElement("div");
       square.classList.add("square");
       square.addEventListener("mouseover", changeColor);
       row.appendChild(square);
@@ -23,5 +32,13 @@ function changeColor (event) {
   const square = event.target;
   square.style.backgroundColor = "#000000";
 }
+
+const redrawButton = document.querySelector("#redraw");
+// redraw board based on user input height and width in squares
+redrawButton.addEventListener("click", () => {
+  const squares = prompt("How many squares high and wide would you like the board to be?");
+  clearBoard();
+  drawBoard(squares);
+});
 
 drawBoard(16); // initial board
