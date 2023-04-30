1d11f606dae4c85998e2c8c433ed619981279189
robot808 <rsbradshaw88@protonmail.com>
2023-04-29 19:40:19 -0500

    Add initial board div and drawBoard function

---
+++ script.js
@@ -0,0 +1,20 @@
+// given number of squares high and wide, draw a squares by squares grid of divs
+function drawBoard (squares) {
+  //rows
+  for (let i = 0; i < squares; i++) {
+    let board = document.querySelector("#board");
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    //columns
+    for (let j = 0; j < squares; j++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+    }
+
+    board.appendChild(row);
+  }
+}
+
+drawBoard(16); // initial board
