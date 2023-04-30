74b483e48acc18e816bf0b41ac1be48f624f619d
robot808 <rsbradshaw88@protonmail.com>
2023-04-30 02:30:58 -0500

    Add paintDarken and paintRandom functions

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ function drawBoard (squares) {
     const row = document.createElement("div");
     row.classList.add("row");
 
-    // columns
+    // squares
     for (let j = 0; j < squares; j++) {
       const square = document.createElement("div");
       square.classList.add("square");
+      square.style.backgroundColor = "#FFFFFF";
       square.addEventListener("mouseover", currentMode);
       row.appendChild(square);
     }
@@ -39,10 +40,41 @@ function redrawBoard () {
   drawBoard(squares);
 }
 
-// given an event, change the background color of the target square div
+// given an event, make the background of the target square div black
 function paintBlack (event) {
   const square = event.target;
   square.style.backgroundColor = "#000000";
+}
+
+// given an event, darken the background of the target square div
+function paintDarken (event) {
+  const square = event.target;
+  // create an array of the current background color rgb values
+  const values = square.style.backgroundColor.slice(4, -1).split(", ");
+  let color = "#";
+
+  // for each value, subtract, convert to hex, and append to color
+  for (let value in values) {
+    value -= 32;
+    if (value < 0) value = 0; // check for negative values
+    if (value < 16) {
+      color += "0" + value.toString(16); // ensure final hex code has six digits
+    }
+    else {
+      color += value.toString(16);
+    }
+  }
+  square.style.backgroundColor = color;
+}
+
+// given an event, make the background of the target square div a random color
+function paintRandom (event) {
+  const square = event.target;
+  let color = "#"
+  for (let i = 0; i < 3; i++) {
+    color += Math.floor(Math.random() * 256).toString(16);
+  }
+  square.style.backgroundColor = color;
 }
 
 // given a mode button's click event, change paint mode accordingly
@@ -65,8 +97,8 @@ function changeMode (event) {
   }
 
   // for each square div, remove old event listener and add a new one
-  board.child.foreach(row => {
-    row.children.foreach(square => {
+  board.childNodes.forEach(row => {
+    row.childNodes.forEach(square => {
       square.removeEventListener("mouseover", currentMode);
       square.addEventListener("mouseover", newMode);
     });
